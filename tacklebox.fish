461c540b3d08f5ab0ab6b63ed49e9abb2a366817
Christopher Larson <kergoth@gmail.com>
2011-11-19 20:05:21 -0700

    Handle tacklebox_theme being unset

--- tacklebox.fish
+++ tacklebox.fish
@@ -1,7 +1,10 @@
 set -g _tb $tacklebox_path
 
 . $_tb/core/core.fish
-. $_tb/themes/$tacklebox_theme.fish
+
+if set -q tacklebox_theme
+    . $_tb/themes/$tacklebox_theme.fish
+end
 
 for plug in $tacklebox_plugins
     for f in $_tb/plugins/$plug/*.fish
