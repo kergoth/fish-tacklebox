22bd6fd036886e8e14dce0fa32355c34103e9eb3
Christopher Larson <kergoth@gmail.com>
2011-11-19 20:05:47 -0700

    Handle complete/function path additions

--- tacklebox.fish
+++ tacklebox.fish
@@ -10,4 +10,10 @@ for plug in $tacklebox_plugins
     for f in $_tb/plugins/$plug/*.fish
         . $f
     end
+    for comp in $_tb/plugins/$plug/completions
+        set fish_complete_path $comp $fish_complete_path
+    end
+    for func in $_tb/plugins/$plug/functions
+        set fish_function_path $func $fish_function_path
+    end
 end
